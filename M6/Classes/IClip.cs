85a1ce12150d3321483db279fd94dd91797ce5e3
charlierobson <charlie.robson@b2m-solutions.com>
2016-01-21 17:04:13 +0000

    goodbyte clip, hello interleaved sample data!

--- M6/Classes/IClip.cs
+++
@@ -1,8 +0,0 @@
-namespace M6.Classes
-{
-    public interface IClip
-    {
-        int SampleFrameCount { get; }
-        IFrameData GetFrames(int start, int count);
-    }
-}
---
+++ M6/Classes/InterleavedSampleData.cs
@@ -0,0 +1,20 @@
+﻿namespace M6.Classes
+{
+    public class InterleavedSampleData : ISampleData
+    {
+        public InterleavedSampleData(int channels, float[] sampleData)
+        {
+            Channels = channels;
+            SampleData = sampleData;
+        }
+
+        public float[] SampleData { get; private set; }
+        public int Channels { get; private set; }
+    }
+
+    public interface ISampleData
+    {
+        float[] SampleData { get; }
+        int Channels { get; }
+    }
+}
